b90b0d2b53094bf28f4e6b6da870d4479eddea9c
looni01 <112870059+looni01@users.noreply.github.com>
2022-09-29 17:25:26 +0200

    Update visualnovel.js

--- visualnovel.js
+++ visualnovel.js
@@ -1,14 +1,18 @@
 let startimg;
+let giff;
 
 function preload() {
   createCanvas(9600, 6000);
   startImg = loadImage("/media/backgrounds/gametitle.png");
+  giff("/media/backgrounds/ui-red.gif");
 }
 
 function draw(){
   background(200,100,2);
-  image(startImg, 0, 0);
+  //image(startImg, 0, 0);
+  image(giff,0,0);
   fill(0);
+    textSize(20);
   text("Hallo", 0,0);
 }
 
