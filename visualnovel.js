77a1612b610c81977f0faeab50224df25954a1fa
looni01 <112870059+looni01@users.noreply.github.com>
2022-09-29 17:01:02 +0200

    Update visualnovel.js

--- visualnovel.js
+++ visualnovel.js
@@ -1,9 +1,13 @@
+let startimg;
+
 function preload() {
   createCanvas(960, 600);
+  startImg = loadImage("/media/backgrounds/gametitle.png");
 }
 
 function draw(){
   background(200,100,2);
+  image(startImg, 0, 0);
   fill(0);
   text("Hallo", 0,0);
 }
